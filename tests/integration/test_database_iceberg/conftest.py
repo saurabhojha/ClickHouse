db95edbb11d3af8424a4675255f46505ff4c95ff
saurabhojha <ojhasaurabh2099@gmail.com>
2025-05-29 00:11:57 +0530

    Parameterize test_iceberg_storage to work with different iceberg rest catalog spec implementations

---
+++ tests/integration/test_database_iceberg/conftest.py
@@ -0,0 +1,9 @@
+def pytest_generate_tests(metafunc):
+    if "catalog_config" in metafunc.fixturenames:
+        metafunc.parametrize(
+            "catalog_config",
+            # TODO: add "nessie_rest" post fixing the issues
+            ["spec_rest"],
+            indirect=True,
+            scope="module"
+        )

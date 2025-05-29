5c1708a520b9f8c768bafda55f102bdc374e9887
saurabhojha <ojhasaurabh2099@gmail.com>
2025-05-29 13:27:47 +0530

    Remove parameterization

--- tests/integration/test_database_iceberg_lakekeeper_catalog/test.py
+++ tests/integration/test_database_iceberg_lakekeeper_catalog/test.py
@@ -6,6 +6,7 @@ import random
 import time
 import uuid
 from datetime import datetime, timedelta
+from dataclasses import dataclass
 
 import pyarrow as pa
 import pytest
@@ -13,6 +14,7 @@ import requests
 import urllib3
 from minio import Minio
 from pyiceberg.catalog import load_catalog
+from pyiceberg.exceptions import NamespaceAlreadyExistsError
 from pyiceberg.partitioning import PartitionField, PartitionSpec
 from pyiceberg.schema import Schema
 from pyiceberg.table.sorting import SortField, SortOrder
@@ -32,11 +34,16 @@ from helpers.s3_tools import get_file_contents, list_s3_objects, prepare_s3_buck
 from helpers.test_tools import TSV, csv_compare
 from helpers.config_cluster import minio_secret_key
 
-BASE_URL = "http://rest:8181/v1"
-BASE_URL_LOCAL = "http://localhost:8182/v1"
-BASE_URL_LOCAL_RAW = "http://localhost:8182"
 
-CATALOG_NAME = "demo"
+@dataclass
+class CatalogConfig:
+    CATALOG_TYPE: str
+    BASE_URL: str
+    BASE_URL_LOCAL: str
+    BASE_URL_LOCAL_RAW: str
+    DOCKER_COMPOSE_FILE: str
+    CATALOG_NAME: str
+
 
 DEFAULT_SCHEMA = Schema(
     NestedField(
@@ -70,20 +77,51 @@ DEFAULT_PARTITION_SPEC = PartitionSpec(
 
 DEFAULT_SORT_ORDER = SortOrder(SortField(source_id=2, transform=IdentityTransform()))
 
-
-def list_namespaces():
-    response = requests.get(f"{BASE_URL_LOCAL}/namespaces")
+def list_namespaces(catalog_confg):
+    response = requests.get(f"{catalog_confg.BASE_URL_LOCAL}/namespaces")
     if response.status_code == 200:
         return response.json()
     else:
         raise Exception(f"Failed to list namespaces: {response.status_code}")
 
+@pytest.fixture(scope="module")
+def catalog_config(request):
+    catalog_type = request.param
+    if catalog_type == "spec_rest":
+        return CatalogConfig(
+            CATALOG_TYPE = "spec_rest",
+            BASE_URL = "http://rest:8181/v1",
+            BASE_URL_LOCAL = "http://localhost:8182/v1",
+            BASE_URL_LOCAL_RAW = "http://localhost:8182",
+            DOCKER_COMPOSE_FILE =  "docker_compose_iceberg_rest_catalog.yml",
+            CATALOG_NAME = "demo"
+        )
+    elif catalog_type == "nessie_rest":
+        return CatalogConfig(
+            CATALOG_TYPE = "nessie_rest",
+            BASE_URL = "http://rest:19120/iceberg/v1",
+            BASE_URL_LOCAL = "http://localhost:19120/iceberg/v1/main",
+            BASE_URL_LOCAL_RAW = "http://localhost:19120/iceberg",
+            DOCKER_COMPOSE_FILE = "docker_compose_iceberg_nessie_rest_catalog.yml",
+            CATALOG_NAME = "demo"
+        )
+    elif catalog_type == "apache_lakekeeper_rest":
+        return CatalogConfig(
+            CATALOG_TYPE = "apache_lakekeeper_rest",
+            BASE_URL = "http://rest:8181/catalog/v1",
+            BASE_URL_LOCAL = "http://localhost:8181/catalog/v1",
+            BASE_URL_LOCAL_RAW = "http://localhost:8181/catalog",
+            DOCKER_COMPOSE_FILE = "docker_compose_iceberg_apache_lake_keeper_catalog.yml",
+            CATALOG_NAME = "demo"
+        )
 
-def load_catalog_impl(started_cluster):
+    raise ValueError(f"Unsupported catalog type: {catalog_type}")
+
+def load_catalog_impl(started_cluster,catalog_config):
     return load_catalog(
-        CATALOG_NAME,
+        catalog_config.CATALOG_NAME,
         **{
-            "uri": BASE_URL_LOCAL_RAW,
+            "uri": catalog_config.BASE_URL_LOCAL_RAW,
             "type": "rest",
             "s3.endpoint": f"http://{started_cluster.get_instance_ip('minio')}:9000",
             "s3.access-key-id": minio_access_key,
@@ -100,13 +138,14 @@ def create_table(
     partition_spec=DEFAULT_PARTITION_SPEC,
     sort_order=DEFAULT_SORT_ORDER,
 ):
-    return catalog.create_table(
+    res = catalog.create_table(
         identifier=f"{namespace}.{table}",
         schema=schema,
         location=f"s3://warehouse-rest/data",
         partition_spec=partition_spec,
         sort_order=sort_order,
     )
+    return res
 
 
 def generate_record():
@@ -120,7 +159,7 @@ def generate_record():
 
 
 def create_clickhouse_iceberg_database(
-    started_cluster, node, name, additional_settings={}
+    started_cluster, node, name,base_url, additional_settings={}
 ):
     settings = {
         "catalog_type": "rest",
@@ -134,7 +173,7 @@ def create_clickhouse_iceberg_database(
         f"""
 DROP DATABASE IF EXISTS {name};
 SET allow_experimental_database_iceberg=true;
-CREATE DATABASE {name} ENGINE = DataLakeCatalog('{BASE_URL}', 'minio', '{minio_secret_key}')
+CREATE DATABASE {name} ENGINE = DataLakeCatalog('{base_url}', 'minio', '{minio_secret_key}')
 SETTINGS {",".join((k+"="+repr(v) for k, v in settings.items()))}
     """
     )
@@ -142,9 +181,24 @@ SETTINGS {",".join((k+"="+repr(v) for k, v in settings.items()))}
     assert minio_secret_key not in show_result
     assert "HIDDEN" in show_result
 
+# For nessie catalog namespaces must be recursively created (e.g. clickhouse_x.testA.A)
+def create_namespace_recursively(catalog, namespace: str):
+    parts = namespace.split(".")
+    for i in range(1, len(parts) + 1):
+        sub_ns = ".".join(parts[:i])
+        try:
+            catalog.create_namespace(sub_ns)
+        except NamespaceAlreadyExistsError as e:
+            logging.info(f"Exception while creating namespace: {str(e)}")
+            pass
+
+def create_catalog_namespace(catalog,catalog_config, namespace: str):
+    if catalog_config.CATALOG_TYPE == "nessie_rest":
+        return create_namespace_recursively(catalog, namespace)
+    return catalog.create_namespace(namespace)
 
 @pytest.fixture(scope="module")
-def started_cluster():
+def started_cluster(catalog_config):
     try:
         cluster = ClickHouseCluster(__file__)
         cluster.add_instance(
@@ -153,6 +207,7 @@ def started_cluster():
             user_configs=[],
             stay_alive=True,
             with_iceberg_catalog=True,
+            extra_parameters={"docker_compose_file_name":catalog_config.DOCKER_COMPOSE_FILE}
         )
 
         logging.info("Starting cluster...")
@@ -167,7 +222,7 @@ def started_cluster():
         cluster.shutdown()
 
 
-def test_list_tables(started_cluster):
+def test_list_tables(started_cluster,catalog_config):
     node = started_cluster.instances["node1"]
 
     root_namespace = f"clickhouse_{uuid.uuid4()}"
@@ -176,13 +231,13 @@ def test_list_tables(started_cluster):
     namespace_1_tables = ["tableA", "tableB"]
     namespace_2_tables = ["tableC", "tableD"]
 
-    catalog = load_catalog_impl(started_cluster)
+    catalog = load_catalog_impl(started_cluster,catalog_config)
 
     for namespace in [namespace_1, namespace_2]:
-        catalog.create_namespace(namespace)
+        create_catalog_namespace(catalog,catalog_config, namespace)
 
     found = False
-    for namespace_list in list_namespaces()["namespaces"]:
+    for namespace_list in list_namespaces(catalog_config)["namespaces"]:
         if root_namespace == namespace_list[0]:
             found = True
             break
@@ -198,7 +253,7 @@ def test_list_tables(started_cluster):
     for namespace in [namespace_1, namespace_2]:
         assert len(catalog.list_tables(namespace)) == 0
 
-    create_clickhouse_iceberg_database(started_cluster, node, CATALOG_NAME)
+    create_clickhouse_iceberg_database(started_cluster, node, catalog_config.CATALOG_NAME,catalog_config.BASE_URL,{})
 
     tables_list = ""
     for table in namespace_1_tables:
@@ -213,27 +268,30 @@ def test_list_tables(started_cluster):
             tables_list += "\n"
         tables_list += f"{namespace_2}.{table}"
 
+    result = node.query(
+        f"SELECT name FROM system.tables WHERE database = '{catalog_config.CATALOG_NAME}' and name ILIKE '{root_namespace}%' ORDER BY name"
+    ).strip()
     assert (
         tables_list
         == node.query(
-            f"SELECT name FROM system.tables WHERE database = '{CATALOG_NAME}' and name ILIKE '{root_namespace}%' ORDER BY name"
+            f"SELECT name FROM system.tables WHERE database = '{catalog_config.CATALOG_NAME}' and name ILIKE '{root_namespace}%' ORDER BY name"
         ).strip()
     )
     node.restart_clickhouse()
     assert (
         tables_list
         == node.query(
-            f"SELECT name FROM system.tables WHERE database = '{CATALOG_NAME}' and name ILIKE '{root_namespace}%' ORDER BY name"
+            f"SELECT name FROM system.tables WHERE database = '{catalog_config.CATALOG_NAME}' and name ILIKE '{root_namespace}%' ORDER BY name"
         ).strip()
     )
 
-    expected = DEFAULT_CREATE_TABLE.format(CATALOG_NAME, namespace_2, "tableC")
+    expected = DEFAULT_CREATE_TABLE.format(catalog_config.CATALOG_NAME, namespace_2, "tableC")
     assert expected == node.query(
-        f"SHOW CREATE TABLE {CATALOG_NAME}.`{namespace_2}.tableC`"
+        f"SHOW CREATE TABLE {catalog_config.CATALOG_NAME}.`{namespace_2}.tableC`"
     )
 
 
-def test_many_namespaces(started_cluster):
+def test_many_namespaces(started_cluster,catalog_config):
     node = started_cluster.instances["node1"]
     root_namespace_1 = f"A_{uuid.uuid4()}"
     root_namespace_2 = f"B_{uuid.uuid4()}"
@@ -247,26 +305,26 @@ def test_many_namespaces(started_cluster):
         f"{root_namespace_2}.CC",
     ]
     tables = ["A", "B", "C"]
-    catalog = load_catalog_impl(started_cluster)
+    catalog = load_catalog_impl(started_cluster,catalog_config)
 
     for namespace in namespaces:
-        catalog.create_namespace(namespace)
+        create_catalog_namespace(catalog,catalog_config, namespace)
         for table in tables:
             create_table(catalog, namespace, table)
 
-    create_clickhouse_iceberg_database(started_cluster, node, CATALOG_NAME)
+    create_clickhouse_iceberg_database(started_cluster, node, catalog_config.CATALOG_NAME,catalog_config.BASE_URL,{})
 
     for namespace in namespaces:
         for table in tables:
             table_name = f"{namespace}.{table}"
             assert int(
                 node.query(
-                    f"SELECT count() FROM system.tables WHERE database = '{CATALOG_NAME}' and name = '{table_name}'"
+                    f"SELECT count() FROM system.tables WHERE database = '{catalog_config.CATALOG_NAME}' and name = '{table_name}'"
                 )
             )
 
 
-def test_select(started_cluster):
+def test_select(started_cluster,catalog_config):
     node = started_cluster.instances["node1"]
 
     test_ref = f"test_list_tables_{uuid.uuid4()}"
@@ -281,10 +339,10 @@ def test_select(started_cluster):
         f"{root_namespace}.A.B.C",
     ]
 
-    catalog = load_catalog_impl(started_cluster)
+    catalog = load_catalog_impl(started_cluster,catalog_config)
 
     for namespace in namespaces_to_create:
-        catalog.create_namespace(namespace)
+        create_catalog_namespace(catalog,catalog_config, namespace)
         assert len(catalog.list_tables(namespace)) == 0
 
     table = create_table(catalog, namespace, table_name)
@@ -294,19 +352,19 @@ def test_select(started_cluster):
     df = pa.Table.from_pylist(data)
     table.append(df)
 
-    create_clickhouse_iceberg_database(started_cluster, node, CATALOG_NAME)
+    create_clickhouse_iceberg_database(started_cluster, node, catalog_config.CATALOG_NAME,catalog_config.BASE_URL,{})
 
-    expected = DEFAULT_CREATE_TABLE.format(CATALOG_NAME, namespace, table_name)
+    expected = DEFAULT_CREATE_TABLE.format(catalog_config.CATALOG_NAME, namespace, table_name)
     assert expected == node.query(
-        f"SHOW CREATE TABLE {CATALOG_NAME}.`{namespace}.{table_name}`"
+        f"SHOW CREATE TABLE {catalog_config.CATALOG_NAME}.`{namespace}.{table_name}`"
     )
 
     assert num_rows == int(
-        node.query(f"SELECT count() FROM {CATALOG_NAME}.`{namespace}.{table_name}`")
+        node.query(f"SELECT count() FROM {catalog_config.CATALOG_NAME}.`{namespace}.{table_name}`")
     )
 
 
-def test_hide_sensitive_info(started_cluster):
+def test_hide_sensitive_info(started_cluster,catalog_config):
     node = started_cluster.instances["node1"]
 
     test_ref = f"test_hide_sensitive_info_{uuid.uuid4()}"
@@ -314,39 +372,41 @@ def test_hide_sensitive_info(started_cluster):
     root_namespace = f"{test_ref}_namespace"
 
     namespace = f"{root_namespace}.A"
-    catalog = load_catalog_impl(started_cluster)
-    catalog.create_namespace(namespace)
+    catalog = load_catalog_impl(started_cluster,catalog_config)
+    create_catalog_namespace(catalog,catalog_config, namespace)
 
     table = create_table(catalog, namespace, table_name)
 
     create_clickhouse_iceberg_database(
         started_cluster,
         node,
-        CATALOG_NAME,
+        catalog_config.CATALOG_NAME,
+        catalog_config.BASE_URL,
         additional_settings={"catalog_credential": "SECRET_1"},
     )
-    assert "SECRET_1" not in node.query(f"SHOW CREATE DATABASE {CATALOG_NAME}")
+    assert "SECRET_1" not in node.query(f"SHOW CREATE DATABASE {catalog_config.CATALOG_NAME}")
 
     create_clickhouse_iceberg_database(
         started_cluster,
         node,
-        CATALOG_NAME,
+        catalog_config.CATALOG_NAME,
+        catalog_config.BASE_URL,
         additional_settings={"auth_header": "SECRET_2"},
     )
-    assert "SECRET_2" not in node.query(f"SHOW CREATE DATABASE {CATALOG_NAME}")
+    assert "SECRET_2" not in node.query(f"SHOW CREATE DATABASE {catalog_config.CATALOG_NAME}")
 
 
-def test_tables_with_same_location(started_cluster):
+def test_tables_with_same_location(started_cluster,catalog_config):
     node = started_cluster.instances["node1"]
 
     test_ref = f"test_tables_with_same_location_{uuid.uuid4()}"
     namespace = f"{test_ref}_namespace"
-    catalog = load_catalog_impl(started_cluster)
+    catalog = load_catalog_impl(started_cluster,catalog_config)
 
     table_name = f"{test_ref}_table"
     table_name_2 = f"{test_ref}_table_2"
 
-    catalog.create_namespace(namespace)
+    create_catalog_namespace(catalog,catalog_config, namespace)
     table = create_table(catalog, namespace, table_name)
     table_2 = create_table(catalog, namespace, table_name_2)
 
@@ -367,7 +427,7 @@ def test_tables_with_same_location(started_cluster):
     df = pa.Table.from_pylist(data)
     table_2.append(df)
 
-    create_clickhouse_iceberg_database(started_cluster, node, CATALOG_NAME)
+    create_clickhouse_iceberg_database(started_cluster, node, catalog_config.CATALOG_NAME,catalog_config.BASE_URL,{})
 
-    assert 'aaa\naaa\naaa' == node.query(f"SELECT symbol FROM {CATALOG_NAME}.`{namespace}.{table_name}`").strip()
-    assert 'bbb\nbbb\nbbb' == node.query(f"SELECT symbol FROM {CATALOG_NAME}.`{namespace}.{table_name_2}`").strip()
+    assert 'aaa\naaa\naaa' == node.query(f"SELECT symbol FROM {catalog_config.CATALOG_NAME}.`{namespace}.{table_name}`").strip()
+    assert 'bbb\nbbb\nbbb' == node.query(f"SELECT symbol FROM {catalog_config.CATALOG_NAME}.`{namespace}.{table_name_2}`").strip()
